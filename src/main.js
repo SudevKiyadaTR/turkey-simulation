ac32076fd6ead6951ba2b7f4b06c16eeabb23bef
Sudev Kiyada <sudev.kiyada@thomsonreuters.com>
2023-03-03 15:19:01 +0530

    fixed camera

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLigh
 
 import studio from '@theatre/studio';
 import { getProject, types } from '@theatre/core';
-import state from '/src/state2.json';
+import state from '/src/state4.json';
 const config = { state };
 var project = null, sheet = null;
 
